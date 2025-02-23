7dc541f9572dea6c8e0c76004e239a0472d70074
bodepop <bodep85@gmail.com>
2025-02-23 12:21:17 +0000

    update

--- fsxtest.py
+++ fsxtest.py
@@ -1,6 +1,6 @@
 import boto3
 client = boto3.client('fsx')
-response = client.describe_file_systems( FileSystemIds=["fs-02962c127de4999a4"])
+response = client.describe_file_systems( FileSystemIds=["fs-id"])
 
 print (response['FileSystems'][0]['Lifecycle'])
 
