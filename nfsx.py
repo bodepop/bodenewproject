57d28fdadcd9b8baa5d417a9a7dd41f02e1c246d
bodepop <bodep85@gmail.com>
2025-02-23 12:22:27 +0000

    update

--- nfsx.py
+++ nfsx.py
@@ -1,6 +1,6 @@
 import boto3
 client = boto3.client('fsx')
-response = client.describe_file_systems( FileSystemIds=["fs-0a7f5db6cde7cf4e2"])
+response = client.describe_file_systems( FileSystemIds=["fsx-id"])
 
 print (response['FileSystems'][0]['Lifecycle'])
 print (response['FileSystems'][0]['FileSystemId'])
