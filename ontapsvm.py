3217db8f783f0825f250a01999c9f24a29aabcba
bodepop <bodep85@gmail.com>
2026-04-03 09:31:19 +0100

    Update ontapsvm.py

--- ontapsvm.py
+++ ontapsvm.py
@@ -1,6 +1,6 @@
 import boto3
 client = boto3.client('fsx')
-response = client.describe_storage_virtual_machines( StorageVirtualMachineIds=["svm-01107a857272f799e"])
+response = client.describe_storage_virtual_machines( StorageVirtualMachineIds=["svm-"])
 
 print (response['StorageVirtualMachines'][0]['FileSystemId'])
 print (response['StorageVirtualMachines'][0]['Lifecycle'])
